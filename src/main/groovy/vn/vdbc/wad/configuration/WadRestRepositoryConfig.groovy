a1d9e1169e2741e8771649214b5e0118b4fd8070
Đỗ Mạnh Thắng <domanhthang@os-MacBook-Air.local>
2025-09-10 11:09:06 +0700

    fix

--- src/main/groovy/vn/vdbc/wad/configuration/WadRestRepositoryConfig.groovy
+++ src/main/groovy/vn/vdbc/wad/configuration/WadRestRepositoryConfig.groovy
@@ -7,7 +7,9 @@ import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer
+import org.springframework.http.HttpMethod
 import org.springframework.web.servlet.config.annotation.CorsRegistry
+import vn.vdbc.wad.model.Post
 
 @Configuration
 class WadRestRepositoryConfig implements RepositoryRestConfigurer {
@@ -21,6 +23,11 @@ class WadRestRepositoryConfig implements RepositoryRestConfigurer {
             Class<?> clazz = entity.getJavaType()
             config.exposeIdsFor(clazz)
         }
+
+        config.getExposureConfiguration()
+                .forDomainType(Post.class)
+                .withItemExposure({ metadata, httpMethods -> httpMethods.disable(HttpMethod.GET) })
+                .withCollectionExposure({ metadata, httpMethods -> httpMethods.disable(HttpMethod.GET) })
     }
 }
 
--- src/main/groovy/vn/vdbc/wad/controller/WadController.groovy
+++ src/main/groovy/vn/vdbc/wad/controller/WadController.groovy
@@ -5,24 +5,22 @@ import org.springframework.data.domain.Page
 import org.springframework.data.domain.PageRequest
 import org.springframework.data.domain.Pageable
 import org.springframework.data.domain.Sort
+import org.springframework.data.rest.webmvc.BasePathAwareController
 import org.springframework.http.ResponseEntity
 import org.springframework.transaction.annotation.Transactional
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
 import vn.vdbc.wad.model.Post
 import vn.vdbc.wad.repository.PostRepo
 
-@RestController
-@RequestMapping("/wad-post")
+@BasePathAwareController
 class WadController {
 
     @Autowired
     PostRepo postRepo
 
-    @GetMapping
+    @GetMapping("wad-post")
     @Transactional(value = "wadTransactionManager", readOnly = true)
     ResponseEntity<?> getAllPosts(
             @RequestParam(name = "page", defaultValue = "0") int page,
@@ -46,7 +44,7 @@ class WadController {
         ])
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("wad-post/{id}")
     @Transactional(value = "wadTransactionManager", readOnly = true)
     ResponseEntity<?> getPostById(@PathVariable(name = "id") Long id) {
         return postRepo.findById(id)
